6ea176f4383e220877207ed6a15ebaa977582a1f
kharon626 <78420479+boost-vladimir112@users.noreply.github.com>
2022-08-14 14:45:19 +0300

    obstacle sound

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private AudioSource _obstacleSound;
+    [SerializeField] private AudioClip _obstacleClip;
+    private void Start()
+    {
+        _obstacleClip = _obstacleSound.clip;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _obstacleSound.PlayOneShot(_obstacleClip);
+
+        }
+    }
+}
